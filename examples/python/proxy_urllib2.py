89118091da60a74cf3c0ecacfddab50efee903c4
Kuaidaili-Dev <32975725+kuaidaili-dev@users.noreply.github.com>
2019-07-13 11:24:13 +0800

    Update proxy_urllib2.py

--- examples/python/proxy_urllib2.py
+++ examples/python/proxy_urllib2.py
@@ -6,11 +6,13 @@
 """
 
 import urllib2
-import base64
 import zlib
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context  # 全局取消证书验证，避免访问https网页报错
 
 #要访问的目标网页
-page_url = "http://dev.kuaidaili.com/testproxy"
+page_url = "http://dev.kdlapi.com/testproxy"
 
 #代理服务器
 proxy = "59.38.241.25:23916"
@@ -19,15 +21,19 @@ proxy = "59.38.241.25:23916"
 username = "myusername"
 password = "mypassword"
 
+proxies = {
+    "http": "http://%(user)s:%(pwd)s@%(ip)s/" % {'user': username, 'pwd': password, 'ip': proxy},
+    "https": "http://%(user)s:%(pwd)s@%(ip)s/" % {'user': username, 'pwd': password, 'ip': proxy}
+}
 
 req = urllib2.Request(page_url)
 req.add_header("Accept-Encoding", "Gzip") #使用gzip压缩传输数据让访问更快
-req.add_header("Proxy-Authorization", "Basic %s" % base64.b64encode('%s:%s' % (username, password)))
-req.set_proxy(proxy, "http")
-#req.set_proxy(proxy, "https")
+proxy_hander = urllib2.ProxyHandler(proxies)
+opener = urllib2.build_opener(proxy_hander)
+urllib2.install_opener(opener)
 r = urllib2.urlopen(req)
 
-print r.code #获取Reponse的返回码
+print r.code
 content_encoding = r.headers.getheader("Content-Encoding")
 if content_encoding and "gzip" in content_encoding:
     print zlib.decompress(r.read(), 16+zlib.MAX_WBITS) #获取页面内容
