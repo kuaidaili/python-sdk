de030f9121e08ed43bcc2deb1021771bb4d98918
Kuaidaili-Dev <32975725+kuaidaili-dev@users.noreply.github.com>
2023-03-22 09:49:18 +0800

    Update middlewares.py

--- examples/http_proxy_tunnel/py3_scrapy/tutorial/middlewares.py
+++ examples/http_proxy_tunnel/py3_scrapy/tutorial/middlewares.py
@@ -104,18 +104,24 @@ class TutorialDownloaderMiddleware:
 
 
 class ProxyDownloaderMiddleware:
+    _proxy = ('XXX.kdlapi.com', '15818')
 
     def process_request(self, request, spider):
-        # 隧道域名:端口号
-        proxy = "XXX.kdlapi.com:15818"
 
         # 用户名密码认证
         username = "username"
         password = "password"
-        request.meta['proxy'] = "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": proxy}
+        request.meta['proxy'] = "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": ':'.join(ProxyDownloaderMiddleware._proxy)}
 
         # 白名单认证
         # request.meta['proxy'] = "http://%(proxy)s/" % {"proxy": proxy}
 
         request.headers["Connection"] = "close"
         return None
+
+    def process_exception(self, request, exception, spider):
+        """捕获407异常"""
+        if "'status': 407" in exception.__str__():  # 不同版本的exception的写法可能不一样，可以debug出当前版本的exception再修改条件
+            from scrapy.resolver import dnscache
+            dnscache.__delitem__(ProxyDownloaderMiddleware._proxy[0])  # 删除proxy host的dns缓存
+        return exception
